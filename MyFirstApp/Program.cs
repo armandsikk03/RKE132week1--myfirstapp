15a541e1d46c3533c06f0686bdde0b89f88f53e7
Armandsikk03 <armand.sikk@tktk.ee>
2024-09-12 17:32:58 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// this is comment
+
+//rakendus küsib küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name:"); //Output
+//string - sõne
+string username =Console.ReadLine();//input
+
+Console.WriteLine("hello" + ", " + username + "!");//Output
+
+//string interpolation - $
+Console.WriteLine($"Hello, {username}!");//output
+
